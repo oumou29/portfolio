78967e1c514e6f67033a0cc91e230f4b0bc2a34a
oumou <151829293+oumou29@users.noreply.github.com>
2024-02-15 08:16:49 +0000

    Add files via upload

---
+++ portfolio/Core/Logic/Header.php
@@ -0,0 +1,32 @@
+<!-- Creation de menu + responisive -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsible Navbar</title>
+    <link rel="stylesheet" href="../Style/Header.css">
+    
+    
+    
+</head>
+<body>
+    
+
+
+    <nav class="navbar">
+    <a href="#" class="Logo">PORTFOLIO</a>
+    <div class="nav-links">
+        <ul>
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Competence</a></li>
+            <li><a href="#">Experience</a></li>
+            <li><a href="#">ProjetAcademique</a></li>
+            <li><a href="#">Veille Technologique</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ portfolio/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="./Core/Style/Header.css">
+    <title>Document</title>
+    
+</head>
+<body>
+<?php include './Core/Logic/Header.php';?>
+    
+   
+</body>
+</html>
